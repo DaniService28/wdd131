047ac0afd2fbb9ee0d54104dec9b989238719727
DaniService28 <her23048@byui.edu>
2025-10-27 09:14:09 -0600

    wq

--- galapagos_recommendations/moreInformation.js
+++ galapagos_recommendations/moreInformation.js
@@ -3,3 +3,30 @@ function getParameterByName(name) {
     const params = new URLSearchParams(search);
     return params.get(name);
 }
+
+const imagenes = {
+  galeria: [
+    'images/bartolome.jpg',
+    'images/floreana.jpg',
+    'images/pinzon.jpg',
+    'images/santafe.jpg'
+  ]
+};
+let currentIndex = 0;
+const imgElement = document.querySelector('.images-container img');
+
+function updateImage() {
+  imgElement.src = imagenes.galeria[currentIndex];
+}
+
+document.getElementById('prev').addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + imagenes.galeria.length) % imagenes.galeria.length;
+    updateImage();
+})
+
+document.getElementById('next').addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % imagenes.galeria.length;
+    updateImage();
+})
+
+updateImage();
